fa6111857c1fd490d254a955218d4e97fe4c24c8
yusufshhzd <yusufshhzd@gmail.com>
2025-01-25 14:01:16 -0500

    getting first csv file with gym data read

---
+++ the-cardio-coders/src/RetrievingData.py
@@ -0,0 +1,14 @@
+import csv
+import json
+
+# Read CSV data
+with open('the-cardio-coders/src/Data/megaGymDataset.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    data = [row for row in csv_reader]
+
+# Convert to JSON
+json_data = json.dumps(data, indent=4)
+
+# Write to JSON file
+with open('megaGymDataSetOutput.json', 'w') as json_file:
+    json_file.write(json_data)
