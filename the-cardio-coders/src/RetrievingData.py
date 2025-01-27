2a0e3186c1c95d33efc0da9bb21d3899e4c8a193
yusufshhzd <yusufshhzd@gmail.com>
2025-01-26 21:07:43 -0500

    redone as a function

--- the-cardio-coders/src/RetrievingData.py
+++ the-cardio-coders/src/RetrievingData.py
@@ -1,14 +1,19 @@
 import csv
 import json
 
-# Read CSV data
-with open('the-cardio-coders/src/Data/megaGymDataset.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    data = [row for row in csv_reader]
 
-# Convert to JSON
-json_data = json.dumps(data, indent=4)
+def getGymDataSet():
+    # Read CSV data
+    with open('the-cardio-coders/src/Data/megaGymDataset.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = [row for row in csv_reader]
 
-# Write to JSON file
-with open('megaGymDataSetOutput.json', 'w') as json_file:
-    json_file.write(json_data)
+    # Convert to JSON
+    json_data = json.dumps(data, indent=4)
+
+    # Write to JSON file
+    with open('megaGymDataSetOutput.json', 'w') as json_file:
+        json_file.write(json_data)
+
+
+
