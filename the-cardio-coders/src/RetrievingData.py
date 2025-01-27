243a9d20f6325118374cc03831aeed62893912b8
yusufshhzd <yusufshhzd@gmail.com>
2025-01-27 11:54:50 -0500

    added more data

--- the-cardio-coders/src/RetrievingData.py
+++ the-cardio-coders/src/RetrievingData.py
@@ -4,6 +4,19 @@ import json
 
 def getGymDataSet():
     # Read CSV data
+    with open('the-cardio-coders/src/Data/gym_exercise_dataset.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = [row for row in csv_reader]
+
+    # Convert to JSON
+    json_data = json.dumps(data, indent=4)
+
+    # Write to JSON file
+    with open('exercises1.json', 'w') as json_file:
+        json_file.write(json_data)
+
+
+        # Read CSV data
     with open('the-cardio-coders/src/Data/megaGymDataset.csv', 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         data = [row for row in csv_reader]
@@ -12,8 +25,24 @@ def getGymDataSet():
     json_data = json.dumps(data, indent=4)
 
     # Write to JSON file
-    with open('megaGymDataSetOutput.json', 'w') as json_file:
+    with open('exercises2.json', 'w') as json_file:
         json_file.write(json_data)
 
 
+
+        # Read CSV data
+    with open('the-cardio-coders/src/Data/stretch_exercise_dataset.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = [row for row in csv_reader]
+
+    # Convert to JSON
+    json_data = json.dumps(data, indent=4)
+
+    # Write to JSON file
+    with open('stretches.json', 'w') as json_file:
+        json_file.write(json_data)
+
+
+
+getGymDataSet()
 
