08df8c03a08381e26da7689f2d69c2009475b681
yusufshhzd <yusufshhzd@gmail.com>
2025-01-30 16:32:24 -0500

    got stretches to insert

--- the-cardio-coders/src/chatBot.py
+++ the-cardio-coders/src/chatBot.py
@@ -5,6 +5,10 @@ from langchain_community.document_loaders import JSONLoader
 from langchain_text_splitters import CharacterTextSplitter
 from pymilvus import Collection, connections
 from uuid import uuid4
+from langchain_text_splitters import RecursiveJsonSplitter
+import json
+from langchain.schema import Document
+
 
 
 ollama = OllamaLLM(model = 'llama3.2')
@@ -14,26 +18,35 @@ embeddings = OllamaEmbeddings(model = "llama3.2")
 vector_store = Milvus(embedding_function=embeddings)
 
 #json files containing data
-raw_json_docs = ["exercises1.json", "exercises2.json", "stretches.json"]
+raw_json_docs = ["stretches.json", "exercises1.json", "exercises2.json"]
 
 documents = []
+num = 0
 
 #split all the files
 for file in raw_json_docs:
     #loading the document
     json_loader = JSONLoader(file, jq_schema=".", text_content=False)
-    doc = json_loader.load()
+    doc = json_loader.load()    
 
     #splitting the document
-    text_splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap=0)
-    split_doc = text_splitter.split_documents(doc)
+    for exercise in doc:
 
-    #add split document to current list
-    documents.extend(split_doc)
+        #getting the page content from json, ignoring metadata
+        exercise_content = exercise.page_content
+
+        #convert json string to list of dictionaries
+        exercise_list = json.loads(exercise_content)
+
+        #add each exercise as an individual document
+        for exercise_item in exercise_list:
+            document = Document(page_content=str(exercise_item), metadata={"source": file, "seq_num": num+1})
+            documents.append(document)
 
 
 #put the documents in the vector store, which acts as the database
 uuids = [str(uuid4()) for _ in range(len(documents))]
+
 vector_store.add_documents(documents=documents, ids=uuids)
 
-print("added docs to db")
+print("reached end")
